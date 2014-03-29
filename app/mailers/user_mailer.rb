025c8d054c48797d4cfc4e6a93382bf28e8a0e37
bonnieli <bonnielokli@gmail.com>
2014-03-29 10:30:16 -0400

    testing email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "bonnniel@gmail.com"
+  default from: "loanify@swaggerloo.ca"
   def new_transaction(email)
   	@e = email
     mail(to: email, subject: 'You have a new transaction!')
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,16 @@ IouApp::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.1and1.com",
+    port: 25,
+    authentication: "plain",
+    user_name: "loanify@swaggerloo.ca",
+    password: "AbC123$",
+    enable_starttls_auto: false
+  }
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
