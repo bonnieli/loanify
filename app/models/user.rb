c10b16213fe4f8268738c1c663fab978c48000a8
yaoxiao4 <yaoxiao4@hotmail.com>
2014-04-13 20:00:40 -0400

    new user function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
 	#add an user
-	def self.iou(input)
+	def self.newuser(input)
 		user = User.new
 		user.first_name = input["first_name"]
-		return Transaction.where("id_b = 1")
+		user.last_name = input["last_name"]
+		user.fb_uniqueid = input["fb_uniqueid"]
+		user.token = input["token"]
+		user.expired = false
+		user.email_address = input["email_address"]
+		return user
 	end
 end
