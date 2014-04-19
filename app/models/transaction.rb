e1e048e827413caccdf7af55bd1b5a61c175a88a
yaoxiao4 <yaoxiao4@hotmail.com>
2014-04-18 21:25:26 -0400

    basic functions of users and transactions
    added some basic functions like add and update and delete

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,82 @@
 class Transaction < ActiveRecord::Base
-	def hello(x)
-		return x
+	
+	def self.newtransaction(input) #new transaction
+		transaction = Transaction.new
+		transaction.paidback = false
+		transaction.reject = false
+		transaction.id_b = input["borrower_key"]
+		transaction.id_l = input["lender_key"]
+		transaction.amount = input["amount"]
+	    transaction.b_email = User.find(input["borrower_key"]).email_address
+	    transaction.l_email = User.find(input["lender_key"]).email_address
+		transaction.save
+		return transaction
 	end
+
+	def self.paidback(input) #payback a transaction
+		transaction = Transaction.find(input["id"])
+		transaction.paidback = true
+		if transaction.transaction_date > input["datepaidback"]
+			return false
+		end
+		transaction.datepaidback = input["datepaidback"]
+		transaction.paidback_time = (transaction.datepaidback - transaction.transaction_date).to_i
+		return transaction.b_email
+	end
+
+	def self.reject(input) #reject a transaction
+		transaction = Transaction.find(input["id"])
+		transaction.reject = true
+		transaction.reject_date = Date.today
+		transaction.reject_reason = input["reject_reason"]
+		return transaction.l_email
+	end
+
+	def self.deletetransaction(input)
+		Transaction.destroy(input)
+	end
+
+#End of all insert/updates/deletes
+
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+
+#split for borrowers
+	def self.borrower_paidback(input)
+		return Transaction.where({id_b: input["id"], paidback: true})
+	end
+
+	def self.borrower_rejected(input)
+		return Transaction.where({id_b: input["id"], rejected: true})
+	end
+
+	def self.borrower_unpaid(input)
+		return Transaction.where({id_b: input["id"], paidback: false, reject: false})
+	end
+
+#split for lenders
+	def self.lender_paidback(input)
+		return Transaction.where({id_l: input["id"], paidback: true})
+	end
+
+	def self.lender_rejected(input)
+		return Transaction.where({id_l: input["id"], rejected: true})
+	end
+
+	def self.lender_unpaid(input)
+		return Transaction.where({id_l: input["id"], paidback: false, reject: false})
+	end
+
+#graphs
+	def self.historygraph(input)
+
+	end
+
+	def self.piechart(input)
+	
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	#add an user
-	def self.newuser(input)
+	def self.newuser(input) #input must have all the fields
 		user = User.new
 		user.first_name = input["first_name"]
 		user.last_name = input["last_name"]
@@ -9,5 +9,25 @@ class User < ActiveRecord::Base
 		user.expired = false
 		user.save
 		return user
+	end
+
+	def self.emailcheck(input) #must be int, user must exist
+		user = User.find(input)
+		if user.email_address == nil
+			return false
+		else
+			return true
+		end
+	end
+
+	def self.addemail(input) # 2 elements, ID and email adderess
+		user = User.find(input["id"])
+		user.email_address = input["email_address"]
+		user.save
+		return user
+	end
+
+	def self.allusers(input)
+		return Users.select("id, email_address, first_name, last_name, profile_picture")
 	end
 end
