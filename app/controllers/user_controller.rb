c847c5bbf375e388c7bea6e454457bc8930bffe7
bonnieli <bonnielokli@gmail.com>
2014-09-18 19:11:47 -0400

    If no transactions ever made (borrower or lender) then charts will not be displayed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,35 +3,39 @@ class UserController < ActionController::Base
   before_action :authenticate_user! #checks if user is signed in
   
   def profile
-    all_charts = {}
+    borrows = Transaction.find_by_id_b(current_user.id)
+    lends = Transaction.find_by_id_l(current_user.id)
 
-   # uri = URI('http://iou.azurewebsites.net/api/values/' + session[:user])
-   # res = Net::HTTP.get(uri)
-   # all_charts.store('Stat', JSON.parse(res))
+    if borrows == nil and lends == nil
+      @empty = true
+      @all_charts = [].to_json
+    else
+      @empty = false
+      all_charts = {}
 
-    #line chart - All
-    res = Transaction.historygraph(current_user.id)
-    all_charts.store('DebtLevel', JSON.parse(res))
+      #line chart - All
+      res = Transaction.historygraph(current_user.id)
+      all_charts.store('DebtLevel', JSON.parse(res))
 
-    #pie chart - Debt
-    res = Transaction.borrower_piechart(current_user.id)
-    all_charts.store('Pie_d', JSON.parse(res))
+      #pie chart - Debt
+      res = Transaction.borrower_piechart(current_user.id)
+      all_charts.store('Pie_d', JSON.parse(res))
 
-    #pie chart - AR
-    res = Transaction.lender_piechart(current_user.id)
-    all_charts.store('Pie_ar', JSON.parse(res))
+      #pie chart - AR
+      res = Transaction.lender_piechart(current_user.id)
+      all_charts.store('Pie_ar', JSON.parse(res))
 
-    #bubble chart - Debt
-    res = Transaction.paidbackhistory(current_user.id)
-    all_charts.store('History_d', JSON.parse(res))
+      #bubble chart - Debt
+      res = Transaction.paidbackhistory(current_user.id)
+      all_charts.store('History_d', JSON.parse(res))
 
-    #bubble chart - AR
-    #uri = URI('http://iou.azurewebsites.net/api/values/History/spaceholder/AR/' + session[:user])
-    #res = Net::HTTP.get(uri)
-    #all_charts.store('History_ar', JSON.parse(res))
+      #bubble chart - AR
+      #uri = URI('http://iou.azurewebsites.net/api/values/History/spaceholder/AR/' + session[:user])
+      #res = Net::HTTP.get(uri)
+      #all_charts.store('History_ar', JSON.parse(res))
 
-    @all_charts = all_charts.to_json
-    puts "@all_charts"
-    puts @all_charts
+      @all_charts = all_charts.to_json
+    end
+
   end
 end
